a054106d5f5b629634c06829abd8c59b511864aa
Kartik saini <kartiksaini@cedcommerce.com>
2022-03-04 18:11:29 +0530

    task done

---
+++ src/addToCart.php
@@ -0,0 +1,198 @@
+<?php
+session_start();
+include("config.php"); 
+ header("location:products.php");
+
+if(!isset($_SESSION['cart'])){
+    $_SESSION['cart'] = array();
+}
+
+$_SESSION['bcheck'] = $_POST['buyNow'];
+
+ if (!isset($_SESSION['buynow']))
+    {
+        $_SESSION['buynow'] =array();
+    }
+
+foreach ($_POST as $k => $v)
+{
+    $action = $k;
+    break;
+}
+switch ($action)
+{  
+    case "AddToCart":
+        addToCart();
+        break;
+
+    case "quantity":
+        changeQuantity();
+        break;
+
+      case "buyNow":
+        buynow(); 
+
+    
+}
+
+
+
+
+
+
+
+
+
+function addToCart()
+{
+    foreach($_SESSION['product'] as $key=> $value)
+    {
+        if (isset($_SESSION['cart']))
+        {
+            foreach($_SESSION['cart'] as $key =>$val){
+                if($val['id']==$_POST['AddToCart']){
+                    $_SESSION['cart'][$key]['quantity'] +=1;
+
+                    return;
+                }
+            }
+        }        
+    }
+
+
+
+
+
+    foreach ($_SESSION['product'] as $value){ 
+        if ($value['id'] == $_POST['AddToCart'])
+        {
+            $id = $value['id'];
+            $name = $value['name'];
+            $image =$value['image'];
+            $price =$value ['price'];
+            $data = array("id" => $id ,"name"=> $name , "image"=> $image ,"price" => $price, "quantity" => "1");
+            array_push($_SESSION['cart'], $data);
+        }
+    }
+   }
+
+   function changeQuantity()
+   {
+       foreach($_SESSION['cart'] as $key =>$val){
+        if($val['id']==$_POST['id']){
+            $_SESSION['cart'][$key]['quantity'] =$_POST['quantity'];
+            return;
+        }
+    }
+   }
+  
+   
+   
+if(isset($_POST['remove'])){
+    foreach($_SESSION['cart'] as $key => $val){
+               if($val['id'] == $_POST['remove']){
+                   array_splice($_SESSION['cart'] , $key ,1);
+                     
+}
+    }
+}
+
+
+function buynow(){
+   
+  
+   
+   foreach($_SESSION['product']  as $key=> $val)
+    {
+        if($_POST['buyNow'] == $val['id'])
+        {
+            $bid = $val['id'];
+            $bname = $val['name'];
+            $bimage =$val['image'];
+            $bprice =$val ['price'];
+            $bdata = array(array("id" => $bid ,"name"=> $bname , "image"=> $bimage ,"price" => $bprice, "quantity" => "1"));
+           $_SESSION['buynow'] = $bdata;
+           break;
+        } 
+       
+    }
+    print_r($_SESSION['buynow']);   
+}
+ 
+if(isset($_POST['addTocart'])){
+    foreach($_SESSION['buynow'] as $key=> $value)
+    {
+        if (isset($_SESSION['cart']))
+        {
+            foreach($_SESSION['cart'] as $key =>$val){
+                if($val['id']==$_POST['addTocart']){
+                 $_SESSION['cart'][$key]['quantity'] +=1;
+                  array_splice($_SESSION['buynow'],$key,1);
+                    return;
+                }
+            }
+        }        
+    }
+    foreach ($_SESSION['buynow'] as $value){ 
+        if ($value['id'] == $_POST['addTocart'])
+        {
+            $id = $value['id'];
+             $name = $value['name'];
+             $image =$value['image'];
+            $price =$value ['price'];
+             $data = array("id" => $id ,"name"=> $name , "image"=> $image ,"price" => $price, "quantity" => "1");
+            array_push($_SESSION['cart'], $data);
+            array_splice($_SESSION['buynow'],$key,1);
+        }
+    }
+
+}
+
+
+
+if(isset($_POST['bbuyNow'])){
+    foreach ($_SESSION['cart'] as $vll){ 
+        if ($vll['id'] == $_POST['bbuyNow'])
+        {
+             $aid = $vll['id'];
+             $aname = $vll['name'];
+             $aimage =$vll['image'];
+            $aprice =$vll ['price'];
+            $aquantity =$vll['quantity'];
+             $adata = array("id" => $aid ,"name"=> $aname , "image"=> $aimage ,"price" => $aprice, "quantity" => $aquantity);
+            array_push($_SESSION['buynow'], $adata);
+            array_splice($_SESSION['cart'],$key,1);
+        }
+    }
+   
+
+}
+
+if(isset($_POST['addtobuy'])){
+
+    
+    foreach($_SESSION['cart'] as $key => $ve){
+              $aid = $ve['id'];
+             $baname = $ve['name'];
+             $baimage =$ve['image'];
+            $baprice =$ve ['price'];
+            $baquantity =$ve['quantity'];
+            $badata = array("id" => $baid ,"name"=> $baname , "image"=> $baimage ,"price" => $baprice, "quantity" => $baquantity);
+            array_push($_SESSION['buynow'], $badata);
+
+            
+            unset($_SESSION['cart']);
+            //array_splice($_SESSION['cart'],$key,1);
+    }
+}
+
+if(isset($_POST['clearcart'])){
+    unset($_SESSION['cart']);
+
+}
+if(isset($_POST['clearBuy'])){
+    unset($_SESSION['buynow']);
+
+}
+
+?>
---
+++ src/products.php
@@ -0,0 +1,126 @@
+<?php
+session_start();
+include("header.php");
+include("config.php");
+//session_destroy();
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>
+		Products
+	</title>
+	<link href="style.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+
+	<div id="main">
+		<div id="products">
+			<?php
+
+
+			foreach ($product as $item) {
+				$des = "<form action='addToCart.php' method='POST'>
+				<div id=" . $item['id'] . " class = 'product'>
+				<img src='images/" . $item['image'] . "'>
+				<h3 class ='title'><a href='#'>Product " . $item['id'] . "</a></h3>
+				<span>Price: $" . $item['price'] . "</span>
+				<button class='add-to-cart' type='submit' name='AddToCart' value=". $item['id'] ." >Add To Cart</button><br>
+				<button class='add-to-cart' type='submit' name='buyNow' value = ".$item['id']." >Buy Now</button>
+				</div>
+				</form>";
+				echo $des;
+			}
+
+
+			?>
+
+		</div>
+	</div>
+	<div>
+
+	</div>
+	<div>
+	<div class ='cartDiv' >
+		<?php
+		if (isset($_SESSION['cart'])) {
+			$html = "<h2>Cart</h2>
+							<table>
+							<tr><th>Name</th><th>Product</th><th>Price</th><th>Quantity</th></tr>";
+			echo ($html);
+		}
+		foreach ($_SESSION['cart'] as $key => $value) {
+
+			$html1 = "<form method='POST' action='addToCart.php'> <tr><td>" . $value['name'] . "</td><td><img  src='images/" . $value['image'] . "' width='60px' height='60px' >
+			</td><td>$" . $value['price'] . "</td><td><input class='edinp' name='quantity' type='number' onchange='form.submit()' value ='" . $value['quantity'] . "'>
+			<input type='hidden' name='id' value=".$value['id'].">
+			<button value=".$value['id']." name='remove' type='Submit'>Remove</button></td></tr>
+			<tr><td></td><td><button type='submit' name='bbuyNow' value=".$value['id'].">Buy Now</button></td><td>$".($value['quantity']*$value['price'])."</td> </tr>";
+			echo $html1;
+		}
+
+		echo "<tr><td></td><td>
+		</td><td></td><td></td>
+		<td><button name='addtobuy'>Buy All</button></td><td><button name='clearcart'>Clear Cart</button></td>
+		</tr></form></table>";
+
+		?>
+	</div>
+	<div class='buydiv'>
+
+
+
+		
+         <?php
+//if (isset($_SESSION['buynow'])) {
+	$html = "<h2>Buy</h2>
+					<table>
+					<tr><th>Name</th><th>Product</th><th>Price</th><th>Quantity</th></tr>";
+	echo ($html);
+//}
+
+
+
+  
+	
+
+
+  
+
+
+ 
+foreach($_SESSION['buynow'] as $key => $val ){
+	 
+if( $_SESSION['buynow'][$key]['id']== $val['id']){
+
+$_SESSION['buynow'][$key]['quantity'] = $val['quantity'];
+	break;
+	}
+
+ 	$html3 = "<form method='POST' action='addToCart.php'> <tr><td>" . $val['name'] . "</td><td><img  src='images/" . $val['image'] . "' width='60px' height='60px' >
+   </td><td>$" . $val['price'] . "</td><td><input class='edinp' name='quantity' type='number' onchange='form.submit()' value ='" . $val['quantity'] . "'>
+ 	<input type='hidden' name='id' value=".$val['id'].">
+    </td></tr>
+    <tr><td></td><td><button type='submit' name= 'addTocart' value=".$val['id'].">Add to Cart</button></td><td>$".($val['quantity']*$val['price'])."</td> </tr>";
+
+ 	echo $html3;
+
+ }
+
+ 
+	
+   echo "<tr><td></td><td>
+		</td><td></td><td></td>
+		<td><button type='submit' name ='clearBuy'> Remove </button></td></form>
+		</tr></table>";
+ 
+		 ?>
+
+
+	</div>
+	</div>
+	<?php include 'footer.php' ?>
